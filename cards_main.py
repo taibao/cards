4843a7708151356bbad2d161d7ade039bcc6aaa8
taibao <2510776295@qq.com>
2019-06-05 21:19:58 +0800

    更新名片系统

--- cards_main.py
+++ cards_main.py
@@ -1,6 +1,10 @@
+#! D:\python
+
+import cards.cards_tools
 # 无限循环，有用户主动退出
 while True:
     # TODO (zhuojiabin)显示功能菜单
+    cards.cards_tools.show_menu()
 
     action_str = input("请选择希望执行的操作：")
     print("您选择的操作是[%s]" % action_str)
@@ -9,12 +13,15 @@ while True:
     if action_str in ["1","2","3"]:
         #新增名片
         if action_str =="1":
+            cards.cards_tools.new_card()
             pass
         #显示全部
         if action_str =="2":
+            cards.cards_tools.show_all()
             pass
         #查询名片
         if action_str =="3":
+            cards.cards_tools.search_card()
             pass
         pass
     #0 退出系统
